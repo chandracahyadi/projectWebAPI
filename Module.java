af81d1dfdf5817a49d0ba1a3cb03a2389c2c70ef
chandracahyadi <57526025+chandracahyadi@users.noreply.github.com>
2019-11-12 08:30:48 +0700

    Create Module.java

---
+++ Module.java
@@ -0,0 +1,25 @@
+package com.entity;
+
+public class Module {
+	
+    private String moduleName;
+    private int moduleOrder;
+
+	public int getModuleOrder() {
+		return moduleOrder;
+	}
+
+	public void setModuleOrder(int moduleOrder) {
+		this.moduleOrder = moduleOrder;
+	}
+
+	public String getModuleName() {
+		return moduleName;
+	}
+
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+    
+    
+}
